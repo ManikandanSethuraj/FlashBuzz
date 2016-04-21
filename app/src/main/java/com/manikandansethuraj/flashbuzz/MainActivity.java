5c93ca85660634749a081e31985c8ee5b51723d4
Manikandan Sethuraj <manikandansethu1987@gmail.com>
2016-04-20 20:29:06 -0400

    Done without errors.
    Signed-off-by: Manikandan Sethuraj <manikandansethu1987@gmail.com>

--- app/src/main/java/com/manikandansethuraj/flashbuzz/MainActivity.java
+++ app/src/main/java/com/manikandansethuraj/flashbuzz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.manikandansethuraj.flashbuzz;
 
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
 
@@ -35,13 +38,39 @@ public class MainActivity extends AppCompatActivity {
                 if (isFlash){
                 if (toggleButton.isChecked()){
 
+                    isOn = toggleButton.isChecked();
+                    Log.d("MainActivity ::::: ", String.valueOf(isOn));
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(parameters);
+                    camera.startPreview();
+
 
                 }else {
+
+                    isOn = toggleButton.isChecked();
+                    Log.d("MainActivity(else)::::", String.valueOf(isOn));
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(parameters);
+                    camera.stopPreview();
 
 
                 }
             }else {
-                    
+
+                    AlertDialog.Builder localBuilder = new AlertDialog.Builder(MainActivity.this);
+                    	localBuilder.setTitle("Error");
+                    	localBuilder.setMessage("Your Device don't have Camera Flash Light !!");
+                    	localBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                finish();
+
+                            }
+                        });
+                    	localBuilder.create();
+                        localBuilder.show();
+
 
                 }
 
@@ -53,5 +82,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+     if (camera != null){
+            camera.release();
+            camera = null;
+       }
+    }
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (camera == null){
+            camera = Camera.open();
+            toggleButton.setChecked(false);
+        }
+    }
 }
